f18a8654e2a1b75cae705ff95051c99811ce9254
Kharanshu <dewankharanshu@gmail.com>
2020-06-15 13:11:57 +0530

    Updates

---
+++ precache-manifest.0f126b00bf91de07a057c9ea52603935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5196143d344b0b7c11ded54e480a38c8",
+    "url": "/TntWatches/index.html"
+  },
+  {
+    "revision": "368054ec8813552887df",
+    "url": "/TntWatches/static/css/main.fac33ac4.chunk.css"
+  },
+  {
+    "revision": "6d3fba509b53a14122ae",
+    "url": "/TntWatches/static/js/2.933200bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TntWatches/static/js/2.933200bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368054ec8813552887df",
+    "url": "/TntWatches/static/js/main.83eca9a6.chunk.js"
+  },
+  {
+    "revision": "68966159721a530156b9",
+    "url": "/TntWatches/static/js/runtime-main.21d00ec7.js"
+  }
+]);
